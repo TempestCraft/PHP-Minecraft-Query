3fc33b73fc9347a8a46aded7ebc1fa6f37f7cda2
chillecharlie <chillecharlie@hotmail.com>
2013-10-30 02:28:08 +0100

    Updated to support 1.6 and 1.7
    Updated to support 1.6 and 1.7

--- MinecraftServerPing.php
+++ MinecraftServerPing.php
@@ -1,17 +1,4 @@
-<?php
-        /*
-         * Queries Minecraft server
-         * Returns array on success, false on failure.
-         *
-         * WARNING: This is using an old "ping" feature, only use this to ping servers prior to 1.7 version.
-         *
-         * Written by xPaw
-         *
-         * Website: http://xpaw.ru
-         * GitHub: https://github.com/xPaw/PHP-Minecraft-Query
-         */
-
-function _QueryMinecraft_Read_VarInt( $Socket )
+function QueryMinecraft_Read_VarInt( $Socket )
 {
 	$i = 0;
 	$j = 0;
@@ -30,9 +17,10 @@ function _QueryMinecraft_Read_VarInt( $Socket )
 	}
 	return $i;
 }
-		 
-function QueryMinecraft( $IP, $Port = 25565, $Timeout = 2 )
+
+function NewQuery( $IP, $Port = 25565, $Timeout = 2 )
 {
+	socket_clear_error();
 	$Socket = Socket_Create( AF_INET, SOCK_STREAM, SOL_TCP );
 	Socket_Set_Option( $Socket, SOL_SOCKET, SO_SNDTIMEO, array( 'sec' => (int)$Timeout, 'usec' => 0 ) );
 	Socket_Set_Option( $Socket, SOL_SOCKET, SO_RCVTIMEO, array( 'sec' => (int)$Timeout, 'usec' => 0 ) );
@@ -56,49 +44,51 @@ function QueryMinecraft( $IP, $Port = 25565, $Timeout = 2 )
 		$Data = Pack( 'cccca*', HexDec( $Length ), 0, 0x04, $Length, $IP ) . Pack( 'nc', $Port, 0x01 );
 		Socket_Send( $Socket, $Data, StrLen( $Data ), 0 ); // handshake
 		Socket_Send( $Socket, "\x01\x00", 2, 0 ); // status ping
-		$Length = _QueryMinecraft_Read_VarInt( $Socket ); // full packet length
+		$Length = QueryMinecraft_Read_VarInt( $Socket ); // full packet length
 		if( $Length < 10 )
 		{
 			Socket_Close( $Socket );
 			return FALSE;
 		}
 		Socket_Read( $Socket, 1 ); // packet type, in server ping it's 0
-		$Length = _QueryMinecraft_Read_VarInt( $Socket ); // string length
+		$Length = QueryMinecraft_Read_VarInt( $Socket ); // string length
 		$Data = Socket_Read( $Socket, $Length, PHP_NORMAL_READ ); // and finally the json string
 		Socket_Close( $Socket );
 		$Data = JSON_Decode( $Data, true );
-	return JSON_Last_Error( ) === JSON_ERROR_NONE ? $Data : FALSE;
+		return JSON_Last_Error( ) === JSON_ERROR_NONE ? $Data : FALSE;
 	}
+	
 	$Data = SubStr( $Data, 3 ); // Strip packet header (kick message packet and short length)
 	$Data = iconv( 'UTF-16BE', 'UTF-8', $Data );
 	// Are we dealing with Minecraft 1.4+ server?
 	if( $Data[ 1 ] === "\xA7" && $Data[ 2 ] === "\x31" )
 	{
 		$Data = Explode( "\x00", $Data );
-		return Array(
-		'HostName'   => $Data[ 3 ],
-		'Players'    => IntVal( $Data[ 4 ] ),
-		'MaxPlayers' => IntVal( $Data[ 5 ] ),
-		'Protocol'   => IntVal( $Data[ 1 ] ),
-		'Version'    => $Data[ 2 ]
-		);
+		$return['description'] = $Data[ 3 ];
+		$return['players']['online'] = IntVal( $Data[ 4 ] );
+		$return['players']['max'] = IntVal( $Data[ 5 ] );
+		$return['version']['protocol'] = IntVal( $Data[ 1 ] );
+		$return['version']['name'] = $Data[ 2 ];
+		return $return;
 	}
 	$Data = Explode( "\xA7", $Data );
-	return Array(
-	'HostName'   => SubStr( $Data[ 0 ], 0, -1 ),
-	'Players'    => isset( $Data[ 1 ] ) ? IntVal( $Data[ 1 ] ) : 0,
-	'MaxPlayers' => isset( $Data[ 2 ] ) ? IntVal( $Data[ 2 ] ) : 0,
-	'Protocol'   => 0,
-	'Version'    => '1.3'
-	);
-	}
-	
-if($Info = QueryMinecraft( '24.132.17.164', '25565' )){
-	$return['MaxPlayers'] = $Info['players']['max'];
-	$return['Players'] = $Info['players']['online'];
-	$return['HostName'] = $Info['description'];
-	$return['Version'] = $Info['version']['name'];
-	print_r($Info);
+	$return['description'] = SubStr( $Data[ 0 ], 0, -1 );
+	$return['players']['online'] = isset( $Data[ 1 ] ) ? IntVal( $Data[ 1 ] ) : 0;
+	$return['players']['max'] = isset( $Data[ 2 ] ) ? IntVal( $Data[ 2 ] ) : 0;
+	$return['version']['protocol'] = 0;
+	$return['version']['name'] = '1.3';
+	return $return;
+}
+
+function QueryMinecraft($Server_IP, $Server_Port = '25565')
+{
+	if($Info = NewQuery( $Server_IP, $Server_Port )){
+		$return['MaxPlayers'] = $Info['players']['max'];
+		$return['Players'] = $Info['players']['online'];
+		$return['HostName'] = $Info['description'];
+		$return['Version'] = $Info['version']['name'];
+		return $return;
 	} else {
-	echo "server offline";
+		return FALSE;
+	}
 }
